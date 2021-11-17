0ee4f446718383bbcaf042e7452cabded02dfabe
yangshuxuan <bighead9851001@163.com>
2021-11-17 10:41:55 +0800

    利用APIView，从函数走上了类，从而为利用继承进行可重用打开了大门

--- store/views.py
+++ store/views.py
@@ -4,66 +4,67 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.views import APIView
 
 from .models import Collection, Product
 
 from .serializers import CollectionSerializer, ProductSerializer
 # Create your views here.
-@api_view(["GET","POST"])
-def product_list(request):
-    if request.method=='GET':
+class ProductList(APIView):
+    def get(self,request):
         queryset = Product.objects.select_related('collection').all()
         serializer = ProductSerializer(queryset,many=True,context={"request":request})
         return Response(serializer.data)
-    elif request.method == 'POST':
+    def post(self,request):
         serializer = ProductSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data,status=status.HTTP_201_CREATED)
 
-@api_view(['GET','PUT','DELETE'])
-def product_detail(request,id):
-    product = get_object_or_404(Product,pk=id)
-    if request.method == 'GET':
+class ProductDetail(APIView):
+    def get(self,request,id):
+        product = get_object_or_404(Product,pk=id)
         serializer = ProductSerializer(product,context={"request":request})
 
         return Response(serializer.data)
-    elif request.method == 'PUT':
+    def put(self,request,id):
+        product = get_object_or_404(Product,pk=id)
         serializer = ProductSerializer(product,data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-    elif request.method == 'DELETE':
+    def delete(self,request,id):
+        product = get_object_or_404(Product,pk=id)
         if product.orderitems.count() > 0:
             return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-@api_view(["GET","POST"])
-def collection_list(request):
-    if request.method == "GET":
+class CollectionList(APIView):
+    def get(self,request):
         queryset = Collection.objects.annotate(products_count=Count("products")).all()
         serializer= CollectionSerializer(queryset,many=True)
         return Response(serializer.data)
-    elif request.method == "POST":
+    def post(self,request):
         serializer = CollectionSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
-@api_view(["GET","DELETE","PUT"])
-def collection_detail(request,pk):
-    collection = get_object_or_404(Collection.objects.annotate(products_count=Count("products")),pk=pk)
-    if request.method == "GET":
-        
+
+class CollectionDetail(APIView):
+    def get(self,request,pk):
+        collection = get_object_or_404(Collection.objects.annotate(products_count=Count("products")),pk=pk)
         serializer = CollectionSerializer(collection)
         return Response(serializer.data)
-    elif request.method == "DELETE":
-        if collection.products_count > 0:
-            return Response({"error":"can't delete it"},status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-    elif request.method == "PUT":
+    def put(self,request,pk):
+        collection = get_object_or_404(Collection.objects.annotate(products_count=Count("products")),pk=pk)
         serializer = CollectionSerializer(collection,data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    def delete(self,request,pk):
+        collection = get_object_or_404(Collection.objects.annotate(products_count=Count("products")),pk=pk)
+        if collection.products_count > 0:
+            return Response({"error":"can't delete it"},status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
