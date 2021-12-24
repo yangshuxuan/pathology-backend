2e9b7f11c5003a40fb3384b6a39eb22fc1d5c9dd
yangshuxuan <bighead9851001@163.com>
2021-12-24 11:48:17 +0800

    增加访问青云对象存储

--- pathology/tasks.py
+++ pathology/tasks.py
@@ -3,13 +3,67 @@ from .models import PathologyPictureItem
 from pathlib import PurePath
 from django.conf import settings
 import subprocess
+import os
+from pathlib import Path
+import hashlib
+from qingstor.sdk.service.qingstor import QingStor
+from qingstor.sdk.config import Config
+
+config = Config('1', '2')
+qingstor = QingStor(config)
+bucket_name='wuyuan'
+bucket = qingstor.Bucket('wuyuan', 'sh1a')
+def readImage(object_key):
+    part_size = 1024 * 1024 * 5  # 5M every part.
+    # ensure the file we will write to does not exists.
+    if os.path.exists(object_key):
+        os.remove(object_key)
+    i = 0
+    while True:
+        lo = part_size * i
+        hi = part_size * (i + 1) - 1
+        byte_range = "bytes=%d-%d" % (lo, hi)
+        output = bucket.get_object(object_key=object_key, range=byte_range)
+        if output.status_code != 206:
+            print("Get object(name: {}) by segment in bucket({}) failed with given message: {}".format(
+                object_key,
+                bucket_name,
+                str(output.content, 'utf-8')))
+            os.remove("/tmp/" + object_key)
+            break
+        else:
+            with open("/tmp/" + object_key, 'a+b') as f:  # append to file in binary mode
+                f.write(output.content)
+            if len(output.content) < part_size:
+                break
+            i += 1
+
+def calculate_md5(filepath) -> str:
+    h = hashlib.md5()
+    with open(filepath, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            h.update(chunk)
+    return h.hexdigest()
 
 @background(schedule=1)
 def notify_user(pathologyPictureItem_id):
     pathologyPictureItem = PathologyPictureItem.objects.get(pk=pathologyPictureItem_id)
-    f = PurePath(pathologyPictureItem.pathologyPicture.path)
-    v = f.parent / f.stem
+    f = PurePath(pathologyPictureItem.pathologyPicture.name)
+    v = f.stem
     subprocess.run([settings.CUT_TOOL, "dzsave",str(f),str(v)])
+    for dirpath, dirnames, files in os.walk(str(v)):
+        print(f'Found directory: {dirpath}')
+        for file_name in files:
+            p=Path(dirpath,file_name)
+            content_md5 = calculate_md5(str(p))
+            object_key = str(p)
+            with p.open(mode="rb") as f:
+                output = bucket.put_object(object_key=object_key, content_type="image/jpeg",content_md5=content_md5,x_qs_storage_class="STANDARD", body=f)
+            if output.status_code != 201:
+                print("Upload object(name: {}) to bucket({}) failed with given message: {}".format(object_key,"wuyuan",str(output.content, 'utf-8')))
+            else:
+                print("Upload success")
+            print(str(p))
 
     pathologyPictureItem.isCutted=True
     pathologyPictureItem.save()
