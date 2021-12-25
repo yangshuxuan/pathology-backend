32d98fac5a195e6c28e7876263eb92313a174e68
yangshuxuan <bighead9851001@163.com>
2021-12-25 21:49:00 +0800

    修改接口测试是否可以读取history

--- pathology/views.py
+++ pathology/views.py
@@ -3,10 +3,13 @@ from django.shortcuts import get_object_or_404
 import xml.etree.ElementTree as ET
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
+
+from pathology.tasks import readImage
 from .models import PathologyPictureItem,LabelItem
 from .serializers import PathologyPictureItemSerializer,LabelItemSerializer
 from rest_framework.decorators import action
 from pathlib import PurePath
+from urllib.parse import urlparse
 from django.utils.encoding import escape_uri_path
 
 # Create your views here.
@@ -18,15 +21,18 @@ class PathologyPictureItemViewSet(ModelViewSet):
     @action(detail=True)
     def history(self,request,pk):
         pathologyPictureItem = PathologyPictureItem.objects.get(pk=pk)
-        f = PurePath(pathologyPictureItem.pathologyPicture.path)
-        v = f.parent / f"{f.stem}.dzi"
-        tree = ET.parse(str(v))
+        f = PurePath(pathologyPictureItem.pathologyPicture.name)
+        v = f"{f.stem}.dzi"
+        readImage(v)
+        tree = ET.parse(v)
         root = tree.getroot()
+        o=urlparse(pathologyPictureItem.pathologyPicture.url)
+        url = o._replace(path=str( f"{f.stem}_files")).geturl()
 
         data = {
             "Image": {
                 "xmlns": "http://schemas.microsoft.com/deepzoom/2009",
-                "Url": f"{request.build_absolute_uri('/')}media/{escape_uri_path(f.stem)}_files/",
+                "Url": url,
                 "Overlap": root.get("Overlap"),
                 "TileSize": root.get("TileSize"),
                 "Format": root.get("Format"),
